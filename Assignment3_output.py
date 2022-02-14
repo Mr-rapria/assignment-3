00aaf9a5032e65cc5a05a79939eba91a31af6242
Mr-rapria <97610540+Mr-rapria@users.noreply.github.com>
2022-02-14 22:29:33 +0530

    Add files via upload

---
+++ Assignment3_output.py
@@ -0,0 +1,73 @@
+QUESTION 1
+ENTER ANY STRING: VICKY
+{'V': 1, 'I': 1, 'C': 1, 'K': 1, 'Y': 1}
+
+
+QUESTION 2
+ENTER THE DAY: 11
+ENTER THE MONTH OF THE YEAR: 5
+ENTER THE YEAR: 2001
+12 / 5 / 2001
+
+
+QUESTION 3
+Enter elements of a list separated by space 5 8 11
+list:  5 8 11
+[(5, 25), (8, 64), (11, 121)]
+
+
+QUESTION 4
+Enter Grade Point: 8
+Your Grade is 'B+' and Very Good performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 20105043
+Enter Name: VICKY
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 20105044
+Enter Name: RAMBO
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 20105045
+Enter Name: BUCKY
+Do you want to enter another student entry(Y or N):N
+<a> {20105043: 'VICKY', 20105044: 'RAMBO', 20105045: 'BUCKY'}
+<b> {20105045: 'BUCKY', 20105044: 'RAMBO', 20105043: 'VICKY'}
+<c> {20105043: 'VICKY', 20105044: 'RAMBO', 20105045: 'BUCKY'}
+Search Name with SID: 20105043
+<d> VICKY
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 12
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 19.333333333333332
+
+
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
